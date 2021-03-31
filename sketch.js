8a7aab7f69eeb9d0061edf768cdcd5211f6ed68c
Shaun <sheenamaryt@gmail.com>
2021-03-31 17:19:34 +0530

    New Commits

---
+++ sketch.js
@@ -0,0 +1,61 @@
+var ground;
+var jumpBox;
+
+var boxVelX, boxVelY;
+
+
+function setup() {
+	createCanvas(displayWidth, displayHeight);
+
+	//Create the Bodies Here.
+
+	ground = createSprite(720,displayHeight,displayWidth+200,displayHeight/10);
+	ground.shapeColor = "yellow";
+
+    jumpBox = createSprite(200,500,50,50);
+	jumpBox.shapeColor = "white";
+	jumpBox.velocityY = 10;
+
+	
+	boxVelX = jumpBox.velocityX;
+	boxVelY = jumpBox.velocityY;
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  boxVelY = mouseY - jumpBox.y;
+  boxVelX = mouseX - jumpBox.x;
+
+  if(collide(ground,jumpBox)){
+	jumpBox.setVelocity(0,0);
+  } else{
+	jumpBox.velocityY = jumpBox.velocityY + 0.9;  
+  }
+
+  if(keyDown("space") && jumpBox.y > 820){
+	jumpBox.velocityY = -10;
+  }
+
+  if(keyDown(UP_ARROW) && jumpBox.y > 820){
+	jumpBox.velocityY = -10;
+  }
+
+  if(keyDown(RIGHT_ARROW)){
+	jumpBox.velocityX = 10
+  }
+
+  if(keyDown(LEFT_ARROW)){
+	jumpBox.velocityX = -10
+  }
+
+  collide(ground,jumpBox);
+  
+  drawSprites();
+}
+
+
+
+
