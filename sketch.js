2fbce7e9d7a7c11cb9a20295fe15308f5b51c9a4
Shaun <sheenamaryt@gmail.com>
2021-03-31 17:29:26 +0530

    fixed bug

--- sketch.js
+++ sketch.js
@@ -56,6 +56,18 @@ function draw() {
   drawSprites();
 }
 
+function collide(sprite1,sprite2){
+  if(sprite1.x - sprite2.x < sprite1.width/2 + sprite2.width/2
+      && sprite2.x - sprite1.x < sprite1.width/2 + sprite2.width/2
+      && sprite1.y - sprite2.y < sprite1.height/2 + sprite2.height/2
+      && sprite2.y - sprite1.y < sprite1.height/2 + sprite2.height/2){
+          return true;
+      }
+      else{
+          return false;
+      }
+}
+
 
 
 
